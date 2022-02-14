6bd815ea7ec583974b9ff432a572c990f7f7b3b1
Hongbusi <q951686662@qq.com>
2022-02-14 23:36:52 +0800

    feat: style

--- components.d.ts
+++ components.d.ts
@@ -5,8 +5,7 @@
 declare module 'vue' {
   export interface GlobalComponents {
     ElButton: typeof import('element-plus/es')['ElButton']
-    HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
   }
 }
 
-export { }
+export {}
