8e6b12d1a7b920681d1d9ca978b87cf39cb9d815
Hongbusi <q951686662@qq.com>
2022-02-14 22:31:23 +0800

    feat(vuex): add vuex

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import router from './router'
+import store from './store'
 
 const app = createApp(App)
 app.use(router)
+app.use(store)
 app.mount('#app')
---
+++ src/store/index.ts
@@ -0,0 +1,14 @@
+import { createStore } from 'vuex'
+
+const store = createStore({
+  state: () => {
+    return {
+      name: 'Hongbusi'
+    }
+  },
+  mutations: {},
+  getters: {},
+  actions: {}
+})
+
+export default store
