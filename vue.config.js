bf3c72c08c356b951fa4351d01217e7f77c1b29f
Hongbusi <q951686662@qq.com>
2022-02-14 23:37:21 +0800

    feat: style

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,7 @@
 // const path = require('path')
 const AutoImport = require('unplugin-auto-import/webpack')
 const Components = require('unplugin-vue-components/webpack')
-const {
-  ElementPlusResolver
-} = require('unplugin-vue-components/resolvers')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
 
 module.exports = {
   // 1. 配置方式一: CLI 提供的属性
