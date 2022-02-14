b5bc724f4afc66d1f4dad4229bc910aa1609cabf
Hongbusi <q951686662@qq.com>
2022-02-14 21:10:30 +0800

    feat: plugin:prettier/recommended

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,8 @@ module.exports = {
     'eslint:recommended',
     '@vue/typescript/recommended',
     '@vue/prettier',
-    '@vue/prettier/@typescript-eslint'
+    '@vue/prettier/@typescript-eslint',
+    'plugin:prettier/recommended'
   ],
   parserOptions: {
     ecmaVersion: 2020
