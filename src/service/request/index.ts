2c0f52ccbf50d65a6f7f57b4fc5804edc7a15565
Hongbusi <q951686662@qq.com>
2022-02-15 23:03:20 +0800

    feat: axios interceptors

--- src/service/request/index.ts
+++ src/service/request/index.ts
@@ -19,10 +19,39 @@ class HRequest {
       this.interceptors?.responseInterceptor,
       this.interceptors?.responseInterceptorCatch
     )
+
+    // 添加所有的实例都有的拦截器
+    this.instance.interceptors.request.use(
+      (config) => {
+        console.log('所有的实例都有的拦截器: 请求拦截成功')
+        return config
+      },
+      (err) => {
+        console.log('所有的实例都有的拦截器: 请求拦截失败')
+        return err
+      }
+    )
+
+    this.instance.interceptors.response.use(
+      (res) => {
+        console.log('所有的实例都有的拦截器: 响应拦截成功')
+        return res
+      },
+      (err) => {
+        console.log('所有的实例都有的拦截器: 响应拦截失败')
+        return err
+      }
+    )
   }
 
-  request(config: AxiosRequestConfig): void {
+  request(config: HRequestConfig): void {
+    if (config.interceptors?.requestInterceptor) {
+      config = config.interceptors.requestInterceptor(config)
+    }
     this.instance.request(config).then((res) => {
+      if (config.interceptors?.responseInterceptor) {
+        res = config.interceptors.responseInterceptor(res)
+      }
       console.log(res)
     })
   }
