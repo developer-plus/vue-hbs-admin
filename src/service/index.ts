d82f402f905de8a203ab8335f88bcc68747c16e0
Hongbusi <q951686662@qq.com>
2022-02-16 10:39:59 +0800

    feat: el-loading

--- src/service/index.ts
+++ src/service/index.ts
@@ -7,10 +7,10 @@ const hRequest = new HRequest({
   timeout: TIME_OUT,
   interceptors: {
     requestInterceptor: (config) => {
+      // 携带 token 的拦截
       const token = ''
-
       if (token) {
-        config.headers.Authorization = `Beare ${token}`
+        // config.headers.Authorization = `Bearer ${token}`
       }
 
       console.log('请求成功的拦截')
--- src/service/request/index.ts
+++ src/service/request/index.ts
@@ -2,9 +2,13 @@ import axios from 'axios'
 import type { AxiosInstance } from 'axios'
 import type { HRequestInterceptors, HRequestConfig } from './type'
 
+import { ElLoading } from 'element-plus'
+import type { LoadingInstance } from 'element-plus/lib/components/loading/src/loading'
+
 class HRequest {
   instance: AxiosInstance
   interceptors?: HRequestInterceptors
+  loadingInstance?: LoadingInstance
 
   constructor(config: HRequestConfig) {
     this.instance = axios.create(config)
@@ -24,6 +28,11 @@ class HRequest {
     this.instance.interceptors.request.use(
       (config) => {
         console.log('所有的实例都有的拦截器: 请求成功拦截')
+        this.loadingInstance = ElLoading.service({
+          lock: true,
+          text: '正在请求数据...',
+          background: 'rgba(0, 0, 0, 0.5)'
+        })
         return config
       },
       (err) => {
@@ -35,6 +44,10 @@ class HRequest {
     this.instance.interceptors.response.use(
       (res) => {
         console.log('所有的实例都有的拦截器: 响应成功拦截')
+
+        // 将 loading 移除
+        // this.loadingInstance?.close()
+
         const data = res.data
         if (data.returnCode === '-1001') {
           console.log('请求失败')
@@ -44,6 +57,7 @@ class HRequest {
       },
       (err) => {
         console.log('所有的实例都有的拦截器: 响应失败拦截')
+        // this.loadingInstance?.close()
         // 例子：判断不同的 HttpErrorCode 显示不同的错误信息
         if (err.response.status === 404) {
           console.log('404 的错误')
