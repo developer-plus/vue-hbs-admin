1e34203d7143a5d00b1aa287077406700da3901c
Hongbusi <q951686662@qq.com>
2022-02-16 12:58:53 +0800

    feat: type export

--- src/service/request/type.ts
+++ src/service/request/type.ts
@@ -1,14 +1,12 @@
 import type { AxiosRequestConfig, AxiosResponse } from 'axios'
 
-interface HRequestInterceptors {
+export interface HRequestInterceptors {
   requestInterceptor?: (config: AxiosRequestConfig) => AxiosRequestConfig
   requestInterceptorsCatch?: (error: any) => any
   responseInterceptor?: (res: AxiosResponse) => AxiosResponse
   responseInterceptorCatch?: (error: any) => any
 }
 
-interface HRequestConfig extends AxiosRequestConfig {
+export interface HRequestConfig extends AxiosRequestConfig {
   interceptors?: HRequestInterceptors
 }
-
-export { HRequestInterceptors, HRequestConfig }
