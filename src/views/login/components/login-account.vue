1e35d3b5761f7c75b56f22ce5ba36d420809753b
Hongbusi <q951686662@qq.com>
2022-02-18 12:56:36 +0800

    feat: account-config

--- src/views/login/components/login-account.vue
+++ src/views/login/components/login-account.vue
@@ -1,10 +1,10 @@
 <template>
-  <el-form ref="accountFormRef" :model="accountForm" :rules="rules">
+  <el-form ref="accountRef" :model="account" :rules="rules">
     <el-form-item label="账号" prop="name">
-      <el-input v-model="accountForm.name"></el-input>
+      <el-input v-model="account.name"></el-input>
     </el-form-item>
     <el-form-item label="密码" prop="password">
-      <el-input v-model="accountForm.password"></el-input>
+      <el-input v-model="account.password"></el-input>
     </el-form-item>
   </el-form>
 </template>
@@ -12,36 +12,14 @@
 <script setup lang="ts">
 import { reactive, ref } from 'vue'
 import type { ElForm } from 'element-plus'
+import { rules } from '../config/account-config'
 
 type FormInstance = InstanceType<typeof ElForm>
 
-const accountFormRef = ref<FormInstance>()
-const accountForm = reactive({
+const accountRef = ref<FormInstance>()
+const account = reactive({
   name: '',
   password: ''
-})
-
-const rules = reactive({
-  name: [
-    {
-      required: true,
-      message: 'Please input Activity name',
-      trigger: 'blur'
-    },
-    {
-      min: 3,
-      max: 5,
-      message: 'Length should be 3 to 5',
-      trigger: 'blur'
-    }
-  ],
-  password: [
-    {
-      required: true,
-      message: 'Please input Activity name',
-      trigger: 'blur'
-    }
-  ]
 })
 
 const submitForm = (formEl: FormInstance | undefined) => {
