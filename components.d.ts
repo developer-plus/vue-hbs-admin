2063c5df7086e609e4e7ae00a94059e585dec0f3
Hongbusi <q951686662@qq.com>
2022-02-20 21:11:41 +0800

    feat: main

--- components.d.ts
+++ components.d.ts
@@ -4,13 +4,18 @@
 
 declare module 'vue' {
   export interface GlobalComponents {
+    ElAside: typeof import('element-plus/es')['ElAside']
     ElButton: typeof import('element-plus/es')['ElButton']
     ElCheckbox: typeof import('element-plus/es')['ElCheckbox']
+    ElContainer: typeof import('element-plus/es')['ElContainer']
+    ElFooter: typeof import('element-plus/es')['ElFooter']
     ElForm: typeof import('element-plus/es')['ElForm']
     ElFormItem: typeof import('element-plus/es')['ElFormItem']
+    ElHeader: typeof import('element-plus/es')['ElHeader']
     ElIcon: typeof import('element-plus/es')['ElIcon']
     ElInput: typeof import('element-plus/es')['ElInput']
     ElLink: typeof import('element-plus/es')['ElLink']
+    ElMain: typeof import('element-plus/es')['ElMain']
     ElTabPane: typeof import('element-plus/es')['ElTabPane']
     ElTabs: typeof import('element-plus/es')['ElTabs']
   }
--- src/views/main/index.vue
+++ src/views/main/index.vue
@@ -1,13 +1,68 @@
 <template>
-  <div>Main</div>
+  <div class="main">
+    <el-container class="main-content">
+      <el-aside width="210px">Aside</el-aside>
+      <el-container class="page">
+        <el-header class="page-header">Header</el-header>
+        <el-main class="page-content">Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  setup() {
-    return {}
-  }
-})
+<script setup lang="ts">
+console.log('fasd')
 </script>
+
+<style scoped lang="less">
+.main {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
+.main-content,
+.page {
+  height: 100%;
+}
+
+.page-content {
+  height: calc(100% - 48px);
+}
+
+.el-header,
+.el-footer {
+  display: flex;
+  align-items: center;
+  text-align: center;
+  color: #333;
+}
+
+.el-header {
+  height: 48px !important;
+}
+
+.el-aside {
+  overflow-x: hidden;
+  overflow-y: auto;
+  line-height: 200px;
+  text-align: left;
+  cursor: pointer;
+  background-color: #001529;
+  transition: width 0.3s linear;
+  scrollbar-width: none;
+  -ms-overflow-style: none;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+}
+
+.el-main {
+  color: #333;
+  text-align: center;
+  background-color: #f0f2f5;
+}
+</style>
