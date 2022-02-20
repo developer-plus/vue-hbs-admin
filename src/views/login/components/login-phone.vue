663d8e11864d9ab9fce053991d81a2edb67d3448
Hongbusi <q951686662@qq.com>
2022-02-20 16:41:58 +0800

    feat: login phone

--- src/views/login/components/login-phone.vue
+++ src/views/login/components/login-phone.vue
@@ -1,42 +1,32 @@
 <template>
-  <el-form ref="phoneRef" :model="phone" :rules="rules">
+  <el-form :model="phone">
     <el-form-item label="手机号" prop="num">
       <el-input v-model="phone.num"></el-input>
     </el-form-item>
     <el-form-item label="验证码" prop="code">
-      <el-input v-model="phone.code"></el-input>
-      <el-button type="primary">获取验证码</el-button>
+      <div class="get-code">
+        <el-input v-model="phone.code"></el-input>
+        <el-button class="get-btn" type="primary">获取验证码</el-button>
+      </div>
     </el-form-item>
   </el-form>
 </template>
 
 <script setup lang="ts">
-import { reactive, ref } from 'vue'
-import type { ElForm } from 'element-plus'
-import { rules } from '../config/account-config'
+import { reactive } from 'vue'
 
-type FormInstance = InstanceType<typeof ElForm>
-
-const accountRef = ref<FormInstance>()
 const phone = reactive({
   num: '',
   code: ''
 })
-
-const submitForm = (formEl: FormInstance | undefined) => {
-  if (!formEl) return
-  formEl.validate((valid) => {
-    if (valid) {
-      console.log('submit!')
-    } else {
-      console.log('error submit!')
-      return false
-    }
-  })
-}
-
-const resetForm = (formEl: FormInstance | undefined) => {
-  if (!formEl) return
-  formEl.resetFields()
-}
 </script>
+
+<style scoped lang="less">
+.get-code {
+  display: flex;
+
+  .get-btn {
+    margin-left: 10px;
+  }
+}
+</style>
