b436e09cc650766a5b92c1f8ed9eaf71aaa19607
Hongbusi <q951686662@qq.com>
2022-02-20 17:33:43 +0800

    feat: show-password

--- src/views/login/components/login-account.vue
+++ src/views/login/components/login-account.vue
@@ -4,7 +4,7 @@
       <el-input v-model="account.name"></el-input>
     </el-form-item>
     <el-form-item label="密码" prop="password">
-      <el-input v-model="account.password"></el-input>
+      <el-input v-model="account.password" show-password></el-input>
     </el-form-item>
   </el-form>
 </template>
