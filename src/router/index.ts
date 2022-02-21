b365f78a3a686193bab1dec4f7806a99899c82ea
Hongbusi <q951686662@qq.com>
2022-02-21 21:25:02 +0800

    feat: not found page

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,11 +10,18 @@ const routes: RouteRecordRaw[] = [
   },
   {
     path: '/login',
+    name: 'login',
     component: () => import('@/views/login/login.vue')
   },
   {
     path: '/main',
+    name: 'main',
     component: () => import('@/views/main/main.vue')
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'not-found',
+    component: () => import('@/views/not-found/not-found.vue')
   }
 ]
 
---
+++ src/views/not-found/not-found.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h2>Not Found</h2>
+  </div>
+</template>
