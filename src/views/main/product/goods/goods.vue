76a813a009d7a724f69e6b6788fed5b841ae2069
Hongbusi <q951686662@qq.com>
2022-02-26 21:22:25 +0800

    feat: table

--- src/views/main/product/goods/goods.vue
+++ src/views/main/product/goods/goods.vue
@@ -1,11 +1,6 @@
 <template>
   <div class="goods">
-    <page-search
-      :search-form-config="searchFormConfig"
-      @handle-reset-click="handleResetClick"
-      @handle-query-click="handleQueryClick"
-    />
-    <page-content :content-table-config="contentTableConfig" page-name="goods" ref="pageContentRef">
+    <page-content :content-table-config="contentTableConfig" page-name="goods">
       <template #image="scoped">
         <el-image style="width: 60px; height: 60px" :src="scoped.row.imgUrl" :preview-src-list="[scoped.row.imgUrl]" />
       </template>
@@ -17,13 +12,7 @@
 </template>
 
 <script setup lang="ts">
-import PageSearch from '@/components/page-search'
 import PageContent from '@/components/page-content'
 
-import { searchFormConfig } from './config/search-config'
 import { contentTableConfig } from './config/content-config'
-
-import { usePageSearch } from '@/hooks/usePageSearch'
-
-const [pageContentRef, handleResetClick, handleQueryClick] = usePageSearch()
 </script>
