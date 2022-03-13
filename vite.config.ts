50f062abf576df0cca43bd33e010892816ed5ff1
Hongbusi <q951686662@qq.com>
2022-03-13 16:30:31 +0800

    chore: config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,24 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
 
 export default defineConfig({
-  plugins: [vue()]
+  resolve: {
+    alias: {
+      '~/': `${path.resolve(__dirname, 'src')}/`
+    }
+  },
+
+  plugins: [
+    vue(),
+
+    // https://github.com/antfu/unplugin-auto-import
+    AutoImport({
+      imports: [
+        'vue'
+      ],
+      dts: 'src/auto-imports.d.ts'
+    }),
+  ]
 })
