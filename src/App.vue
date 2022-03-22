0743c2e2e9cf26c2af91965d5369048ab29f463e
Hongbusi <q951686662@qq.com>
2022-03-22 11:50:17 +0800

    chore: update

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,3 @@
 <template>
   <router-view />
 </template>
-
-<script setup lang="ts">
-import { accountLoginRequest } from './api/login'
-
-accountLoginRequest({ name: 'Hongbusi', password: 'admin' }).then((res) => {
-  console.log(res)
-})
-</script>
