8f88a9300b7c5ad69eb090f8ae6e07eff6607705
Hongbusi <q951686662@qq.com>
2022-03-23 17:17:24 +0800

    chore: animation

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import '@unocss/reset/tailwind.css'
+import './styles/index.css'
 import 'uno.css'
 
 import router from './router'
--- unocss.config.js
+++ unocss.config.js
@@ -16,6 +16,26 @@ export default defineConfig({
     transformerVariantGroup()
   ],
   rules: [
-    ['min-w-1280', { 'min-width': '1280px' }]
+    ['min-w-1280', { 'min-width': '1280px' }],
+    [/^enter-(x|y)-([1-9])/, ([, d, index]) => {
+      return {
+        'transform': `translate${d.toUpperCase()}(50px)`,
+        'z-index': `${10 - index}`,
+        'opacity': '0',
+        'animation': `enter-${d}-animation 0.4s ease-in-out 0.3s`,
+        'animation-fill-mode': 'forwards',
+        'animation-delay': `${(index * 1) / 10}s`
+      }
+    }],
+    [/^-enter-(x|y)-([1-9])/, ([, d, index]) => {
+      return {
+        'transform': `translate${d.toUpperCase()}(-50px)`,
+        'z-index': `${10 - index}`,
+        'opacity': '0',
+        'animation': `enter-${d}-animation 0.4s ease-in-out 0.3s`,
+        'animation-fill-mode': 'forwards',
+        'animation-delay': `${(index * 1) / 10}s`
+      }
+    }]
   ]
 })
