bc75cef192990261ad703151950b54249f57c018
Hongbusi <q951686662@qq.com>
2022-03-24 17:09:50 +0800

    chore: set token

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,17 @@
 import Request from '../utils/request'
 import { BASE_URL, TIME_OUT } from './config'
+import localCache from '~/utils/cache'
+import { TOKEN_KEY } from '~/enums/cacheEnum'
 
 const request = new Request({
   baseURL: BASE_URL,
   timeout: TIME_OUT,
   interceptorHooks: {
     requestInterceptor: (config) => {
+      const token = localCache.getCache(TOKEN_KEY)
+      if (token) {
+        config.headers.Authorization = token
+      }
       return config
     },
     requestInterceptorCatch: (error) => {
