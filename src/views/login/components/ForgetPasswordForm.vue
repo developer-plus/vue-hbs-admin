61e638011ab634f59341fc1da946780b9755e6d1
Hongbusi <q951686662@qq.com>
2022-03-28 16:43:57 +0800

    chore: reset password

--- src/views/login/components/ForgetPasswordForm.vue
+++ src/views/login/components/ForgetPasswordForm.vue
@@ -20,23 +20,6 @@
       <a-input-password v-model:value="formData.password" size="large" />
     </a-form-item>
 
-    <a-row class="enter-x">
-      <a-col :span="12">
-        <a-form-item class="enter-x">
-          <a-checkbox v-model:checked="remember" size="small">
-            记住密码
-          </a-checkbox>
-        </a-form-item>
-      </a-col>
-      <a-col :span="12">
-        <a-form-item :style="{ 'text-align': 'right' }" class="enter-x">
-          <a-button type="link" size="small" @click="setLoginState(LoginStateEnum.RESET_PASSWORD)">
-            忘记密码?
-          </a-button>
-        </a-form-item>
-      </a-col>
-    </a-row>
-
     <a-form-item class="enter-x">
       <a-button size="large" type="primary" block :loading="loading" @click="handleReset">
         重置
@@ -53,14 +36,6 @@
 
 <script setup lang="ts">
 import type { Form } from 'ant-design-vue'
-
-import {
-  GithubFilled,
-  WechatFilled,
-  AlipayCircleFilled,
-  GoogleCircleFilled,
-  TwitterCircleFilled
-} from '@ant-design/icons-vue'
 
 import { LoginStateEnum, useLoginState } from '../useLogin'
 import { useUserStore } from '~/stores/modules/user'
