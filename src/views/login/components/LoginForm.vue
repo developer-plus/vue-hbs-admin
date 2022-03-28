af9abdb911fe673d4e87a64a97fcb5dd04c2710c
Hongbusi <q951686662@qq.com>
2022-03-28 15:52:43 +0800

    chore: login

---
+++ src/views/login/components/LoginForm.vue
@@ -0,0 +1,107 @@
+<template>
+  <a-form
+    :model="formState"
+    name="basic"
+
+    autocomplete="off"
+    @finish="onFinish"
+  >
+    <a-form-item
+      class="enter-x"
+      name="username"
+      :rules="[{ required: true, message: 'Please input your username!' }]"
+    >
+      <a-input v-model:value="formState.username" size="large" />
+    </a-form-item>
+
+    <a-form-item
+      class="enter-x"
+      name="password"
+      :rules="[{ required: true, message: 'Please input your password!' }]"
+    >
+      <a-input-password v-model:value="formState.password" size="large" />
+    </a-form-item>
+
+    <a-row class="enter-x">
+      <a-col :span="12">
+        <a-form-item class="enter-x" name="remember">
+          <a-checkbox v-model:checked="formState.remember" size="small">
+            记住密码
+          </a-checkbox>
+        </a-form-item>
+      </a-col>
+      <a-col :span="12">
+        <a-form-item :style="{ 'text-align': 'right' }" class="enter-x" name="remember">
+          <a-button type="link" size="small">
+            忘记密码?
+          </a-button>
+        </a-form-item>
+      </a-col>
+    </a-row>
+
+    <a-form-item class="enter-x">
+      <a-button size="large" type="primary" html-type="submit" block>
+        登录
+      </a-button>
+    </a-form-item>
+
+    <a-row class="enter-x" justify="space-between">
+      <a-col :span="7">
+        <a-button block>
+          手机登录
+        </a-button>
+      </a-col>
+      <a-col :span="7">
+        <a-button block>
+          手机登录
+        </a-button>
+      </a-col>
+      <a-col :span="7">
+        <a-button block>
+          手机登录
+        </a-button>
+      </a-col>
+    </a-row>
+
+    <a-divider class="enter-x">
+      其他登录方式
+    </a-divider>
+
+    <div class="flex justify-evenly enter-x sign-in-way">
+      <GithubFilled />
+      <WechatFilled />
+      <AlipayCircleFilled />
+      <GoogleCircleFilled />
+      <TwitterCircleFilled />
+    </div>
+  </a-form>
+</template>
+
+<script setup lang="ts">
+import {
+  GithubFilled,
+  WechatFilled,
+  AlipayCircleFilled,
+  GoogleCircleFilled,
+  TwitterCircleFilled
+} from '@ant-design/icons-vue'
+
+import { useUserStore } from '~/stores/modules/user'
+
+interface FormState {
+  username: string
+  password: string
+  remember: boolean
+}
+
+const formState = reactive<FormState>({
+  username: 'Hongbusi',
+  password: 'admin',
+  remember: true
+})
+
+const user = useUserStore()
+const onFinish = (values: any) => {
+  user.loginAction(values)
+}
+</script>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,86 +1,34 @@
 <template>
-  <div class="hbs-login h-full">
+  <div class="hbs-login h-full flex justify-center items-center">
     <Plum />
 
-    <div class="flex h-full">
-      <div class="flex-1">
-        left
-      </div>
-      <div class="flex-1 flex justify-center items-center">
-        <a-form
-          :model="formState"
-          name="basic"
-          :label-col="{ span: 8 }"
-          :wrapper-col="{ span: 16 }"
-          autocomplete="off"
-          @finish="onFinish"
-        >
-          <a-form-item
-            label="Username"
-            name="username"
-            :rules="[{ required: true, message: 'Please input your username!' }]"
-          >
-            <a-input v-model:value="formState.username" />
-          </a-form-item>
-
-          <a-form-item
-            label="Password"
-            name="password"
-            :rules="[{ required: true, message: 'Please input your password!' }]"
-          >
-            <a-input-password v-model:value="formState.password" />
-          </a-form-item>
-
-          <a-form-item name="remember" :wrapper-col="{ offset: 8, span: 16 }">
-            <a-checkbox v-model:checked="formState.remember">
-              Remember me
-            </a-checkbox>
-          </a-form-item>
-
-          <a-form-item :wrapper-col="{ offset: 8, span: 16 }">
-            <a-button type="primary" html-type="submit">
-              Submit
-            </a-button>
-          </a-form-item>
-        </a-form>
-      </div>
+    <div class="w-400px">
+      <LoginForm />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { useUserStore } from '~/stores/modules/user'
-
-interface FormState {
-  username: string
-  password: string
-  remember: boolean
-}
-
-const formState = reactive<FormState>({
-  username: 'Hongbusi',
-  password: 'admin',
-  remember: true
-})
-
-const user = useUserStore()
-const onFinish = (values: any) => {
-  user.loginAction(values)
-}
+import LoginForm from './components/LoginForm.vue'
 </script>
 
-<style scoped lang="less">
-.hbs-login::before {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  margin-left: -48%;
-  background-image: url(/src/assets/svg/login-bg.svg);
-  background-position: 100%;
-  background-repeat: no-repeat;
-  background-size: auto 100%;
-  content: '';
+<style lang="less">
+.hbs-login {
+  .ant-divider-inner-text {
+    font-size: 12px;
+    color: #00000073;
+  }
+
+  .sign-in-way {
+    .anticon {
+      font-size: 22px;
+      color: #888;
+      cursor: pointer;
+
+      &:hover {
+        color: #0960bd;
+      }
+    }
+  }
 }
 </style>
