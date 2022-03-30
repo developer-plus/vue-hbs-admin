571948a1b90133fd3a1767941282738f2656f706
Hongbusi <q951686662@qq.com>
2022-03-30 15:15:05 +0800

    chore: optimization

--- build/plugins/vue-components.ts
+++ build/plugins/vue-components.ts
@@ -4,7 +4,7 @@ import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 
 export default function setupVueComponents() {
   return Components({
-    dirs: ['src/global-components'],
+    dirs: ['src/components'],
     extensions: ['vue'],
     include: [/\.vue$/, /\.vue\?vue/],
     dts: 'types/components.d.ts',
