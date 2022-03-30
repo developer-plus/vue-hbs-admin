becafa41c8783c2748e96525e6939eaf268c2959
Hongbusi <q951686662@qq.com>
2022-03-30 14:20:16 +0800

    chore: update

--- mock/_util.ts
+++ mock/_util.ts
@@ -1,4 +1,9 @@
-// Interface data format used to return a unified format
+export interface requestParams {
+  method: string
+  body: any
+  headers?: { authorization?: string }
+  query: any
+}
 
 export function resultSuccess<T = Recordable>(data: T, { message = 'ok' } = {}) {
   return {
@@ -18,42 +23,6 @@ export function resultError(message = 'Request failed', { code = -1, result = nu
   }
 }
 
-export function resultPageSuccess<T = any>(
-  page: number,
-  pageSize: number,
-  list: T[],
-  { message = 'ok' } = {}
-) {
-  const pageData = pagination(page, pageSize, list)
-
-  return {
-    ...resultSuccess({
-      items: pageData,
-      total: list.length
-    }),
-    message
-  }
-}
-
-export function pagination<T = any>(pageNo: number, pageSize: number, array: T[]): T[] {
-  const offset = (pageNo - 1) * Number(pageSize)
-  const ret
-    = offset + Number(pageSize) >= array.length
-      ? array.slice(offset, array.length)
-      : array.slice(offset, offset + Number(pageSize))
-  return ret
-}
-
-export interface requestParams {
-  method: string
-  body: any
-  headers?: { authorization?: string }
-  query: any
-}
-
-/**
- * @description 本函数用于从 request 数据中获取 token，请根据项目的实际情况修改
- */
 export function getRequestToken({ headers }: requestParams): string | undefined {
   return headers?.authorization
 }
