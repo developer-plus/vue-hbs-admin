e4359b9a7dc43d7c610a41a0a73363dea9669f4a
Hongbusi <q951686662@qq.com>
2022-04-01 15:49:28 +0800

    feat: user dropdown

--- src/layouts/default/header/components/UserDropdown.vue
+++ src/layouts/default/header/components/UserDropdown.vue
@@ -1,5 +1,30 @@
 <template>
-  <div>
-    Hongbusi
-  </div>
+  <a-dropdown>
+    <div>
+      <a-avatar :src="logoUrl" />
+      <span class="ml-10px text-base text-black">Hongbusi</span>
+    </div>
+
+    <template #overlay>
+      <a-menu>
+        <a-menu-item key="lock">
+          <span class="flex items-center">
+            <lock-outlined class="mr-1" />
+            <span>锁定屏幕</span>
+          </span>
+        </a-menu-item>
+        <a-menu-item key="logout">
+          <span class="flex items-center">
+            <poweroff-outlined class="mr-1" />
+            <span>退出登录</span>
+          </span>
+        </a-menu-item>
+      </a-menu>
+    </template>
+  </a-dropdown>
 </template>
+
+<script setup lang="ts">
+import { LockOutlined, PoweroffOutlined } from '@ant-design/icons-vue'
+import logoUrl from '~/assets/logo.png'
+</script>
