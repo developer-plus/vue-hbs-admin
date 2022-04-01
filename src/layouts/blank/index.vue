1dbb25d0c97cff5a06042d31930f9527c3dbf9d9
Hongbusi <q951686662@qq.com>
2022-04-01 15:52:14 +0800

    feat: blank layout

--- src/layouts/blank/index.vue
+++ src/layouts/blank/index.vue
@@ -1,1 +1,9 @@
-<template>blank</template>
+<template>
+  <div class="h-full">
+    <router-view v-slot="{ Component, route }">
+      <keep-alive>
+        <component :is="Component" :key="route.fullPath" />
+      </keep-alive>
+    </router-view>
+  </div>
+</template>
