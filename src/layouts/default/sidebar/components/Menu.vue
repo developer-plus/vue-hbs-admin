1c4088a9de98a0fec31a9c337e36552a47fe9580
Hongbusi <q951686662@qq.com>
2022-04-04 21:52:52 +0800

    chore: update

--- src/layouts/default/sidebar/components/Menu.vue
+++ src/layouts/default/sidebar/components/Menu.vue
@@ -6,66 +6,30 @@
     theme="dark"
     :inline-collapsed="getCollapsed"
   >
-    <a-menu-item key="1">
-      <template #icon>
-        <MailOutlined />
-      </template>
-      Navigation One
-    </a-menu-item>
-    <a-menu-item key="2">
-      <template #icon>
-        <CalendarOutlined />
-      </template>
-      Navigation Two
-    </a-menu-item>
     <a-sub-menu key="sub1">
       <template #icon>
         <AppstoreOutlined />
       </template>
       <template #title>
-        Navigation Three
+        Demo
       </template>
-      <a-menu-item key="3">
-        Option 3
+      <a-menu-item key="3" @click="handleClick('/demo/excel/import')">
+        Import Excel
       </a-menu-item>
-      <a-menu-item key="4">
-        Option 4
+      <a-menu-item key="4" @click="handleClick('/demo/excel/export')">
+        Export Excel
       </a-menu-item>
-      <a-sub-menu key="sub1-2" title="Submenu">
-        <a-menu-item key="5">
-          Option 5
-        </a-menu-item>
-        <a-menu-item key="6">
-          Option 6
-        </a-menu-item>
-      </a-sub-menu>
     </a-sub-menu>
-    <a-sub-menu key="sub2">
+    <a-menu-item key="7" @click="handleClick('/about')">
       <template #icon>
         <SettingOutlined />
       </template>
-      <template #title>
-        Navigation Four
-      </template>
-      <a-menu-item key="7">
-        Option 7
-      </a-menu-item>
-      <a-menu-item key="8">
-        Option 8
-      </a-menu-item>
-      <a-menu-item key="9">
-        Option 9
-      </a-menu-item>
-      <a-menu-item key="10">
-        Option 10
-      </a-menu-item>
-    </a-sub-menu>
+      关于
+    </a-menu-item>
   </a-menu>
 </template>
 <script setup lang="ts">
 import {
-  MailOutlined,
-  CalendarOutlined,
   AppstoreOutlined,
   SettingOutlined
 } from '@ant-design/icons-vue'
@@ -76,4 +40,10 @@ const { getCollapsed } = useCollapsed()
 
 const selectedKeys = ref(['1'])
 const openKeys = ref(['sub1'])
+
+const router = useRouter()
+
+const handleClick = (path: string) => {
+  router.push(path)
+}
 </script>
