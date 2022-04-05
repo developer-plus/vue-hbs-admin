20d32ca453f2711dd524e80e95b9955761152bfe
Hongbusi <q951686662@qq.com>
2022-04-05 11:23:46 +0800

    docs: add vitepress

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,40 @@
+import { defineConfig } from 'vitepress'
+
+const nav = [
+  {
+    text: '贡献指南',
+    link: '/contribution-guide'
+  },
+]
+
+const sidebar = {}
+
+export default defineConfig({
+  lang: 'zh-CN',
+  title: 'vue-hbs-admin',
+  description: '一个基于 Vue3.0、Vite、TypeScript、Ant-Design-Vue 的后台解决方案，目标是为中小型项目开发，提供现成的开箱解决方案及丰富的示例。',
+  base: '/',
+  srcDir: 'src',
+
+  head: [],
+
+  themeConfig: {
+    repo: 'Hongbusi/vue-hbs-admin',
+    docsDir: 'docs/src',
+    docsBranch: 'master',
+
+    editLinks: true,
+    editLinkText: 'Edit this page on GitHub',
+    lastUpdated: 'Last Updated',
+
+    nav,
+
+    sidebar,
+  },
+
+  vite: {
+    define: {
+      __VUE_OPTIONS_API__: false
+    }
+  }
+})
---
+++ docs/src/index.md
@@ -0,0 +1,23 @@
+---
+home: true
+---
+
+# vue-hbs-admin
+
+一个基于 Vue3.0、Vite、TypeScript、Ant-Design-Vue 的后台解决方案，目标是为中小型项目开发，提供现成的开箱解决方案及丰富的示例。
+
+## Demo
+
+[在线预览](https://hongbusi.github.io/vue-hbs-admin)
+
+## 目标
+
+- 做成一个让自己引以为傲、无可挑剔的项目。
+- 提供丰富的解决方案，便于以后项目中可以开箱即用。
+- 组建一个开源项目小团队，一起学习、敲代码、互相 review，提高水平。
+- 熟悉参加开源项目流程。
+- 200 star。
+
+## 加入小组
+
+添加微信：Hongbusi16530，备注：开源小组。
