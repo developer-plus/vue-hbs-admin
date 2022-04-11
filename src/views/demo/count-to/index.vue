bf75ff5dbbd5a105335f206382a3c2bce026972b
Hongbusi <q951686662@qq.com>
2022-04-11 18:48:34 +0800

    feat(count-to): update demo

--- src/views/demo/count-to/index.vue
+++ src/views/demo/count-to/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <a-card title="数字动画">
+  <a-card title="数字动画（自动）">
     <a-space>
       <a-button @click="endVal += 1000">
         +1000
@@ -9,17 +9,56 @@
       </a-button>
     </a-space>
 
-    <div>
-      <a-card-grid style="width: 25%; text-align: center">
-        <count-to :start-val="startVal" :end-val="endVal" />
+    <div class="mt-20px">
+      <a-card-grid style="width: 50%; text-align: center">
+        <count-to
+          :start-val="startVal"
+          :end-val="endVal"
+          prefix="$"
+        />
+      </a-card-grid>
+      <a-card-grid style="width: 50%; text-align: center">
+        <count-to
+          :start-val="startVal"
+          :end-val="endVal"
+          suffix="元"
+          color="red"
+        />
+      </a-card-grid>
+    </div>
+  </a-card>
+
+  <a-card title="数字动画（手动）">
+    <a-space>
+      <a-button @click="countToRef.start()">
+        开始
+      </a-button>
+      <a-button @click="countToRef.reset()">
+        重置
+      </a-button>
+    </a-space>
+
+    <div class="mt-20px">
+      <a-card-grid style="width: 100%; text-align: center">
+        <count-to
+          ref="countToRef"
+          :start-val="startVal"
+          :end-val="endVal"
+          :autoplay="false"
+          suffix="元"
+          color="red"
+        />
       </a-card-grid>
     </div>
   </a-card>
 </template>
 
 <script setup lang="ts">
+// eslint-disable-next-line @typescript-eslint/consistent-type-imports
 import { CountTo } from '~/components/CountTo'
 
 const startVal = ref(0)
 const endVal = ref(6666)
+
+const countToRef = ref<InstanceType<typeof CountTo> | null>(null)
 </script>
