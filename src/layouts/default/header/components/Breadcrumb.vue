9de3f22037570b35ec7ba10a00785c1d05e4337a
一只海星 <78190504+kongcodes@users.noreply.github.com>
2022-04-30 23:16:21 +0800

    feat: add breadcrumb navigation (#74)
    * feat: breadcrumb navigation
    
    * style: delete empty lines and useless elements

---
+++ src/layouts/default/header/components/Breadcrumb.vue
@@ -0,0 +1,17 @@
+<template>
+  <a-breadcrumb class="!ml-2">
+    <a-breadcrumb-item v-for="(item,index) in routes" :key="index">
+      {{ item.meta.title }}
+    </a-breadcrumb-item>
+  </a-breadcrumb>
+</template>
+
+<script setup lang="ts">
+const route = useRoute()
+const router = useRouter()
+const routes = ref(route.matched)
+
+router.afterEach((to) => {
+  routes.value = to.matched
+})
+</script>
